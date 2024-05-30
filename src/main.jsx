bc02b99442c442af57e8ba31d6ab54f7f8eba87b
Holovko Maksym <golovkomaksim852@gmail.com>
2024-05-30 11:02:25 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import App from "./App.jsx";
 import "modern-normalize";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
+import { store, persistor } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
